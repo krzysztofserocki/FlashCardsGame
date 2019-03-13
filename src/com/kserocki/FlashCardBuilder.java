be5b2df2eec5cc061e1037e437ac7033773c215d
krzysztofserocki <leicer141@gmail.com>
2019-03-13 16:47:07 +0100

    Created textAreas, labels and buttons

--- src/com/kserocki/FlashCardBuilder.java
+++ src/com/kserocki/FlashCardBuilder.java
@@ -1,4 +1,71 @@
 package com.kserocki;
 
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 public class FlashCardBuilder {
+
+    private JTextArea question;
+    private JTextArea answer;
+    private ArrayList<FlashCard> cardList;
+    private JFrame frame;
+
+    public FlashCardBuilder() {
+        // Build the User interface
+        frame = new JFrame("Flash Card");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create a JPanel to hold everything
+        JPanel mainPanel = new JPanel();
+
+        // Create amd setup font
+        Font greatFont = new Font("Helvetica Neue", Font.BOLD, 21);
+        // Question font area
+        question = new JTextArea(6, 20);
+        question.setLineWrap(true);
+        question.setWrapStyleWord(true);
+        question.setFont(greatFont);
+        // Answer font area
+        answer = new JTextArea(6, 20);
+        answer.setLineWrap(true);
+        answer.setWrapStyleWord(true);
+        answer.setFont(greatFont);
+
+        // JScrollPane question
+        JScrollPane questionScrollPane = new JScrollPane(question);
+        questionScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        questionScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        // JScrollPane answer
+        JScrollPane answerScrollPane = new JScrollPane(answer);
+        answerScrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        answerScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        // Setting up buttons
+        JButton nextButton = new JButton("Next Card");
+
+        // Create a few labels
+        JLabel questionLabel = new JLabel("Question");
+        JLabel answerLabel = new JLabel("Answer");
+
+        // Add components to mainPanel
+        mainPanel.add(questionLabel);
+        mainPanel.add(questionScrollPane);
+        mainPanel.add(answerLabel);
+        mainPanel.add(answerScrollPane);
+        mainPanel.add(nextButton);
+
+
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FlashCardBuilder();
+            }
+        });
+    }
 }
--- src/com/kserocki/FlashCardPlayer.java
+++ src/com/kserocki/FlashCardPlayer.java
@@ -1,5 +1,5 @@
 package com.kserocki;
 
 public class FlashCardPlayer {
-    
+
 }
